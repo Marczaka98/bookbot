b33ed1fc1db23bc7a9f16a1444f9282d57c391e3
Marczaka98 <austin.marczak98@gmail.com>
2024-05-29 23:45:13 -0400

    added sort_on function to main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
-    #print(text)
     word_count = count_words(text)
     letter_counts = count_letters(text)
-    print(f"WORD COUNT: {word_count}")
-    #print(count_letters(text))
-    convert_letter_dict_to_list(letter_counts)
+    converted_list = convert_letter_dict_to_list(letter_counts)
+    converted_list.sort(reverse=True, key=sort_on)
+    print(converted_list)
 
 def get_text(path):
     with open(path) as f:
@@ -34,6 +33,9 @@ def convert_letter_dict_to_list(letter_dict):
             letter_list.append(
                 {"letter": letter, "count": letter_dict[letter]}
             )
-    print(letter_list)
+    return letter_list
+
+def sort_on(dict):
+    return dict["count"]
 
 main()
